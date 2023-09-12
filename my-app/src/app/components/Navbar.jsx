449bcb5f746756deb9e7a9bbaf44bd760c977999
loarden <biacsi.ors@gmail.com>
2023-09-12 16:45:51 +0200

    creating next project

---
+++ my-app/src/app/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="max-w-7xl mx-auto flex justify-between">
+      <h1>NextChat</h1>
+      <button>Login</button>
+    </nav>
+  )
+}
